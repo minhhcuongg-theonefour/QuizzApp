30c40dc02b2dc46dcc1a2e0254eb61aa47e414cf
Thomas <cuongnguyen14122001@gmail.com>
2023-08-10 05:48:00 +0700

    connect to firebase & fetch topic data

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:quizzapp/services/firestore.dart';
+import 'package:quizzapp/services/models.dart';
 import 'package:quizzapp/shared/bottom_nav.dart';
 
 class TopicPage extends StatelessWidget {
@@ -6,8 +9,38 @@ class TopicPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Data is loading');
+        } else if (snapshot.hasError) {
+          return Center(
+            // child: ErrorMessage(message: snapshot.error.toString()),
+            child: Text(snapshot.error.toString()),
+          );
+        } else if (snapshot.hasData) {
+          var topics = snapshot.data!;
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: const Text('Topics'),
+            ),
+            body: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: topics.map((topic) => Text(topic.title)).toList(),
+
+            ),
+            bottomNavigationBar: const BottomNavBar(),
+          );
+        } else {
+          return const Text('No topics found in Firestore. Check database');
+        }
+      },
     );
   }
 }
